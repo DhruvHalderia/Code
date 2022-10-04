243f4f81c05c09fa48ec7565e0bfd7db23590b01
Dhruv Halderia <dhalderia@gmail.com>
2022-10-03 19:02:42 -0500

    bal nums

---
+++ balancing_numbers.py
@@ -0,0 +1,9 @@
+# By submitting this assignment, I agree to the following:
+#   "Aggies do not lie, cheat, or steal, or tolerate those who do."
+#   "I have not given or received any unauthorized aid on this assignment."
+#
+# Name:         DHRUV HALDERIA
+# Section:      503
+# Assignment:   LAB 6.15
+# Date:         3 October 2022
+#
