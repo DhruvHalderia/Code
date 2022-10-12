42f3d26dd8473e788cd4db614a968ac8050806fe
Dhruv Halderia <dhalderia@gmail.com>
2022-10-12 14:21:42 -0500

    final changes

--- go_moves.py
+++ go_moves.py
@@ -19,15 +19,18 @@ while going != "stop":
            print(x, end = " ")
        print()
     #this gets the grid position that player one wants to put a stone at 
-    gridVal = int(float(input("What grid do you want to change player one? ")))
-    placeVal = int(float(input("What value within the grid player one? ")))
+    gridVal = int(float(input("What grid do you want to change, player one? ")))
+    placeVal = int(float(input("What value within the grid, player one? ")))
     #this detects if there is a stone at that position already 
     if(grid[gridVal][placeVal] != "."):
         while(grid[gridVal][placeVal] != "."):
+            for i in range(len(grid[0])):
+                for x in grid[i]:
+                    print(x, end = " ")
+                print()
             print("This spot is already taken, please enter a new spot down below")
-            gridVal = int(float(input("What grid do you want to change player one? ")))
-            placeVal = int(float(input("What value within the grid player one? ")))
-            print()
+            gridVal = int(float(input("What grid do you want to change, player one? ")))
+            placeVal = int(float(input("What value within the grid, player one? ")))
     #below the grid changes the value the player wants to put their stone at 
     grid[gridVal][placeVal] = chr(9675)
     #this will print the grid with the stone placed there 
@@ -36,14 +39,18 @@ while going != "stop":
             print(x, end = " ")
         print()
     #this gets the grid position that player two wants to put a stone at 
-    gridVal = int(float(input("What grid do you want to change player two? ")))
-    placeVal = int(float(input("What value within the grid player two? ")))
+    gridVal = int(float(input("What grid do you want to change, player two? ")))
+    placeVal = int(float(input("What value within the grid, player two? ")))
     #this detects if there is a stone at that position already 
     if(grid[gridVal][placeVal] != "."):
         while(grid[gridVal][placeVal] != "."):
+            for i in range(len(grid[0])):
+                for x in grid[i]:
+                    print(x, end = " ")
+                print()
             print("This spot is already taken, please enter a new spot down below")
-            gridVal = int(float(input("What grid do you want to change player two? ")))
-            placeVal = int(float(input("What value within the grid player two? ")))
+            gridVal = int(float(input("What grid do you want to change, player two? ")))
+            placeVal = int(float(input("What value within the grid, player two? ")))
             print()
     #below the grid changes the value the player wants to put their stone at
     grid[gridVal][placeVal] = chr(9679)
