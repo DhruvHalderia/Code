40a361f112629f851223c33945b963f579e4444d
Dhruv Halderia <dhalderia@gmail.com>
2022-10-24 12:52:29 -0500

    ascii clock complete

--- ascii_clock.py
+++ ascii_clock.py
@@ -11,11 +11,13 @@
 #time = input("Enter the time: ")
 #we get the input from the user with the time variable
 time = input("Enter the time: ")
+print()
 #we get the numbers in the dictionary with the object being a list
-numbers = {"0":["000","0 0","0 0","0 0","000"],"1": [" 1 ", "11 ", " 1 ", " 1 ", "111"], "2": ["222", "  2", "222", "2  ", "222"], "3":["333", "  3", "333", "  3", "333"] ,"4": ["4 4", "4 4","444","  4","  4"], "5": ["555", "5  ", "555", "  5", "555"], "6": ["666","6  ","666","6 6","666"], "7": ["777","  7","  7","  7","  7"],"8":["888", "8 8","888","8 8","888"], "9":["999","9 9","999","  9","  9"], ":": ["   "," : ","   "," : ","   "]}
+numbers = {"0":["000","0 0","0 0","0 0","000"],"1": [" 1 ", "11 ", " 1 ", " 1 ", "111"], "2": ["222", "  2", "222", "2  ", "222"], "3":["333", "  3", "333", "  3", "333"] ,"4": ["4 4", "4 4","444","  4","  4"], "5": ["555", "5  ", "555", "  5", "555"], "6": ["666","6  ","666","6 6","666"], "7": ["777","  7","  7","  7","  7"],"8":["888", "8 8","888","8 8","888"], "9":["999","9 9","999","  9","999"], ":": [" ",":"," ",":"," "]}
 #we print it with a for loop going through the dictionary with the numbers
 for i in range(5):
+#this is a if statement to determine the 4 or 3 digit number
     if len(time) == 5:
-        print(numbers[time[0:1]][i],"",numbers[time[1:2]][i],"",numbers[time[2:3]][i],"",numbers[time[3:4]][i],"",numbers[time[4:5]][i])
+        print(numbers[time[0:1]][i],""+numbers[time[1:2]][i],""+numbers[time[2:3]][i],""+numbers[time[3:4]][i],""+numbers[time[4:5]][i],"") #this is for the four digit number 
     else:
-        print(numbers[time[0:1]][i],"",numbers[time[1:2]][i],"",numbers[time[2:3]][i],"",numbers[time[3:4]][i])
+        print(numbers[time[0:1]][i],""+numbers[time[1:2]][i],""+numbers[time[2:3]][i],""+numbers[time[3:4]][i],"") #this if for the 3 digit number 
---
+++ ascii_clock_DH.py
@@ -0,0 +1,26 @@
+# asking user to enter a time (e.g. "12:36" or "1:42")
+# prints a space
+# this makes the shape of "0"
+           # this makes the shape of "1"
+           # this makes the shape of "2"
+           # this makes the shape of "3"
+           # this makes the shape of "4"
+           # this makes the shape of "5"
+           # this makes the shape of "6"
+           # this makes the shape of "7"
+           # this makes the shape of "8"
+           # this makes the shape of "9"
+           # this makes the shape of ":"
+
+#for loop assesses each digit of the user's input
+#range is 5 because the user's input has a maximum of 5 characters including the 
+        #if the user's input has four digits
+        #if the user's input has three digits
+        
+
+if len(time) == 5:
+    #this bottom statement will get 4 numbers with the right spacing and then it splices the time string for that number or character and then goes through the list for that key value and prints each individual line by itself
+        print(numbers[time[0:1]][i],""+numbers[time[1:2]][i],""+numbers[time[2:3]][i],""+numbers[time[3:4]][i],""+numbers[time[4:5]][i],"") #this is for the four digit number 
+    else:
+    #this bottom statement will get 4 numbers with the right spacing and then it splices the time string for that number or character and then goes through the list for that key value and prints each individual line by itself
+        print(numbers[time[0:1]][i],""+numbers[time[1:2]][i],""+numbers[time[2:3]][i],""+numbers[time[3:4]][i],"") #this if for the 3 digit number 
