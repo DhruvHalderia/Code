6aafe3534edb41e8af4c279ddedd8c539360766e
Dhruv Halderia <dhalderia@gmail.com>
2022-11-14 14:27:08 -0600

    coins Complete

--- count_coins.py
+++ count_coins.py
@@ -5,23 +5,38 @@
 # Name:         DHRUV HALDERIA
 # Section:      503
 # Assignment:   LAB 11.12
-# Date:         11/13/22
+# Date:         11/14/22
 #
 #This makes the coins variable and output and user file
 coins = 0 
 outputFile = open('coins.txt', 'w')
 userFile = open('game.txt','r')
 #this goes through every line in the code
+fileList = []
 for i in userFile.readlines():
-    listOfCoins = i.split(' ')
+    fileList.append(i)
+
+parseNum = 0
+while parseNum < len(fileList):
 #the if statements determine if the coin is valid
-    if listOfCoins[0]=='coin' or listOfCoins[0] == 'jump':
-        if listOfCoins[1][0:2] != '+0':
-            coins+=1
-            if listOfCoins[1][0] == '+':
-                outputFile.write(listOfCoins[1][1:])
-            else: 
-                outputFile.write(listOfCoins[1])
+    if 'jump' in fileList[parseNum]:
+        if '+' in fileList[parseNum]:
+            parseNum += int(fileList[parseNum][6:])
+        else:
+            parseNum -= int(fileList[parseNum][6:])
+    elif 'coin' in fileList[parseNum]:
+        if '+' in fileList[parseNum]:
+            outputFile.write(fileList[parseNum][6:])
+            coins+= int(fileList[parseNum][6:])
+            parseNum+=1
+        else:
+            outputFile.write(fileList[parseNum][5:])
+            coins-= int(fileList[parseNum][6:])
+            parseNum+=1
+    else:
+        parseNum+=1
+    
+    
 print(f'Total coins collected: {coins}')
 userFile.close()
 outputFile.close()
