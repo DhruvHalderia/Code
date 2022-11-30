aa75faaec8a6f9e69120f805a3f3d47e5c65aa72
Dhruv Halderia <dhalderia@gmail.com>
2022-11-30 12:08:48 -0600

    test

--- test3.py
+++ test3.py
@@ -1,4 +1,11 @@
-string1 = "Howdy ags welcome to engineering"
-list = string1.split()
-print(list)
-print(list[:-1])
+import numpy as np
+arr2 =[]
+arr = np.arange(6).reshape(6//3,3)
+ 
+for i in range(1,7):
+    arr[(i-1)//3][(i-1)%3] = i
+max = 0
+for i in arr:
+    if i[0]> max:
+        max = i[0]
+print(max)
