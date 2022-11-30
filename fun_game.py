141567073e5b9ec6e98fa7b7d1235d950fddac3d
Dhruv Halderia <dhalderia@gmail.com>
2022-11-29 19:24:38 -0600

    gameFun

--- fun_game.py
+++ fun_game.py
@@ -11,13 +11,33 @@
 import random 
 def makeBoard():
 #This makes the initial board for the user
-    top = ['B',"I","N","G","O"]
-    bottom = ['B',"I","N","G","O"]
-    total = [top,bottom]
-    print(total)
+    top = [' B'," I"," N"," G"," O"]
+    total = [top]
+    for i in range(5):
+        row =[]
+        for b in range(5):
+            temp = random.randint(1,99)
+            if temp < 10:
+                temp = '0'+str(temp)
+            row += [temp]
+        total += [row]
+    total[3][2] = 'xx'  
+    printBoard(total)
+    return total
+def printBoard(x):
+    for i in x:
+        for b in i:
+            print(b, end=' ')
+        print()
 
+def getRandomNumbers():
+    print()
 
 
 
 print("------Welcome to Bingo------\n--------Instructions--------\n----You get a free space----\n---Try to get 5 in a row----\n--Rows can be horizontal,---\n---diagonal, and vertical---\n-----------Enjoy!-----------")
-makeBoard()
+gameBoard = makeBoard()
+print()
+userInput = int(input("Enter 1 to start the game "))
+while userInput == 1: 
+    print()
--- test.py
+++ test.py
@@ -1,10 +1,7 @@
-from matplotlib import pyplot as plt
-plt.rcParams["figure.figsize"] = [7.00, 3.50]
-plt.rcParams["figure.autolayout"] = True
-x = [4,5,6,7]
-y = [3,2,5,7]
-plt.xlim(0, 7)
-plt.ylim(0, 7)
-plt.grid()
-plt.plot(x, y,'r')
-plt.show()
+number = input("Enter a number: ")  # assume the user enters 5.0 
+try: 
+    x = int(number) 
+    print(f"x is the integer {x}") 
+except: 
+    x = float(number) 
+    print(f"x is the float {x}") 
